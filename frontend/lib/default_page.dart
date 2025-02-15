055096207cfc3152dbbeab8ef22ced3492458102
Koolaidpick <dat90189911@gmail.com>
2025-02-15 15:40:10 -0800

    Implemented rough skeleton of main page
    Co-authored-by: tyler-ham05 <tyler-ham05@users.noreply.github.com>

---
+++ frontend/lib/default_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DefaultPage extends StatelessWidget {
+  const DefaultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Pick a hobby to get started'),
+    ); 
+  }
+}
